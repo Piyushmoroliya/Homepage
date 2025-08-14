14903b09330d9b1cd418eae17a2f4f8e31a88540
Piyush Moroliya <pmoroliya4@gmail.com>
2025-08-14 21:04:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+// Togggle navigation For header
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('navLinks');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+// Dropdown menu
+
+
